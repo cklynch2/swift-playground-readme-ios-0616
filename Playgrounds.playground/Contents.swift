f2f5a37856d88507c14711d05f019e765a8d581c
cklynch2 <cklynch2@gmail.com>
2016-07-05 14:17:30 -0400

    Learning about how to use playground to better understand what my code is doing and how variables change over the course of a program

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,18 @@
 let str = "Hello, world"
-str = "Goodbye, world"
+
+// str = "Goodbye, world"
+
+var iOSClassSize = 34
+var iOSPizzaSlicesPerPerson = 2
+
+let totalPizzaSlices = iOSClassSize * iOSPizzaSlicesPerPerson
+
+// Someone is super hungry after learning all this new stuff about swift! Better order more pizza...
+iOSPizzaSlicesPerPerson = 3
+
+// Someone dropped out of the class because they only want to write code in Objective C. Silly head!
+iOSClassSize = 33
+
+// Even though total pizza slices was defined as the product of two variables, classSize and pizzaSlicesPerPerson, and even though we reassigned the values of those variables, the constant totalPizzaSlices does NOT change. 
+print(totalPizzaSlices)
+
